584b4502e47ec6377c5baf89a8bfe2d2df09b422
KimiyaKhoshnood <kimiya.khoshnod@gmail.com>
2025-02-10 12:03:08 +0330

    updating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
+import "@/app/globals.css";
 import Footer from "@/Components/Footer/Footer";
 
 const geistSans = Geist({
