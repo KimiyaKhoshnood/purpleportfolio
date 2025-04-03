c604ff364cff500909a869937b55fbebea0a167e
Kimia Khoshnood <160478888+KimiyaKhoshnood@users.noreply.github.com>
2025-04-03 16:25:09 +0330

    Update DownloadResumeBtn.tsx

--- src/Components/HeroSection/DownloadResumeBtn.tsx
+++ src/Components/HeroSection/DownloadResumeBtn.tsx
@@ -1,12 +1,12 @@
-"use client"
+// "use client"
 
 const DownloadResumeBtn = () => {
-    const handleDownload = () => {
-        const link = document.createElement('a');
-        link.href = '/KimiaKhoshnoodResume.pdf';
-        link.download = 'KimiaKhoshnoodResume.pdf';   
-        link.click();
-      };
+    // const handleDownload = () => {
+    //     const link = document.createElement('a');
+    //     link.href = '/KimiaKhoshnoodResume.pdf';
+    //     link.download = 'KimiaKhoshnoodResume.pdf';   
+    //     link.click();
+    //   };
     
   return (
     <button disabled className="py-3 px-7 flex w-fit rounded-3xl font-medium border border-[#8750F7] hover:bg-[#8750F7] hover:text-white transition-all duration-200 ease-linear cursor-pointer">
